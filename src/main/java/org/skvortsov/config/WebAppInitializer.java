80e6b4a7330b665162fcb0ae604fa20753cbe007
fedorskvortsov <skvortsovfk@gmail.com>
2022-02-24 18:35:31 +0300

    initial commit

---
+++ src/main/java/org/skvortsov/config/WebAppInitializer.java
@@ -0,0 +1,35 @@
+package org.skvortsov.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+@Slf4j
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("onStartup()");
+
+        // Create the spring application context
+        AnnotationConfigWebApplicationContext webApplicationContext =
+                new AnnotationConfigWebApplicationContext();
+        webApplicationContext.register(WebConfig.class);
+
+        // Create the dispatcher servlet
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+
+        // Register and configure the dispatcher servlet
+        ServletRegistration.Dynamic registration =
+                servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcherServlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
