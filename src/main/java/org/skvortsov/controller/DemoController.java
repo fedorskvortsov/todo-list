e3d5b60583aea4be5c5dbbace679d2eea7a9c8f6
fedorskvortsov <skvortsovfk@gmail.com>
2022-02-25 13:35:37 +0300

    feat: add demo service and request params

--- src/main/java/org/skvortsov/controller/DemoController.java
+++ src/main/java/org/skvortsov/controller/DemoController.java
@@ -1,13 +1,26 @@
 package org.skvortsov.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.skvortsov.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
-    // http://localhost:8080/todo-list/hello
+    private final DemoService demoService;
+
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
@@ -15,10 +28,22 @@ public class DemoController {
     }
 
     // http://localhost:8080/todo-list/welcome
-    // prefix + name + suffix
-    // /WEB-INF/view/welcome.jsp
+    // http://localhost:8080/todo-list/welcome?user={user}
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
+        log.info("model = {}", model);
+
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
         return "welcome";
+    }
+
+    // http://localhost:8080/todo-list/hello
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return demoService.getWelcomeMessage();
     }
 }
---
+++ src/main/java/org/skvortsov/service/DemoService.java
@@ -0,0 +1,8 @@
+package org.skvortsov.service;
+
+public interface DemoService {
+
+    String getHelloMessage(String user);
+
+    String getWelcomeMessage();
+}
