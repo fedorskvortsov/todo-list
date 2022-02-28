e0be1b31ed8b8123d247b0baf61040433207cfa5
fedorskvortsov <skvortsovfk@gmail.com>
2022-02-28 09:32:22 +0300

    fix: Todo item constructor

--- src/main/java/org/skvortsov/model/TodoItem.java
+++ src/main/java/org/skvortsov/model/TodoItem.java
@@ -7,11 +7,16 @@ import lombok.EqualsAndHashCode;
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class TodoItem {
     private int id;
     private String title;
     private String details;
     private LocalDate deadline;
+
+    public TodoItem(String title, String details, LocalDate deadline) {
+        this.title = title;
+        this.details = details;
+        this.deadline = deadline;
+    }
 }
